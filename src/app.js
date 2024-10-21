ebdd79d29da768cc41cba81a5669271a46ac32cf
Michaelrick88 <michaelrick88@hotmail.com>
2024-10-21 19:28:59 +0000

    [michaelrick88 generador de excusas halloween]
    realice el generador de excusas en js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,29 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "El fantasma",
+    "Mi hermano disfrazado",
+    "La bruja vecina",
+    "El zombie"
+  ];
+  let action = ["asustó", "robó", "encantó", "desapareció"];
+  let what = ["mis caramelos", "mi disfraz", "la calabaza", "mi linterna"];
+  let when = [
+    "antes de la fiesta",
+    "cuando estaba buscando dulces",
+    "mientras decoraba",
+    "durante la noche de brujas"
+  ];
+  function generador() {
+    let randonwho = who[Math.floor(Math.random() * who.length)];
+    let randonaction = action[Math.floor(Math.random() * action.length)];
+    let randonwhat = what[Math.floor(Math.random() * what.length)];
+    let randonwhen = when[Math.floor(Math.random() * when.length)];
+
+    return randonwho + " " + randonaction + " " + randonwhat + " " + randonwhen;
+  }
+  //console.log("Hello Rigo from the console!");
+  let parrafo = document.getElementById("excuse");
+  parrafo.innerText = generador();
 };
